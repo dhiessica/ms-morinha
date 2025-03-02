c79f8306b8e6e5b6bf4b44681b5689279cd3a4d0
dhiessicamoreira <dhiessicamoreira@hotmail.com>
2025-03-02 10:39:44 -0300

    Adicionar dependencias spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,13 @@ dependencies {
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("com.h2database:h2")
+
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	val jjwt = "0.12.6"
+	implementation("io.jsonwebtoken:jjwt-api:${jjwt}")
+	implementation("io.jsonwebtoken:jjwt-jackson:${jjwt}")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:${jjwt}")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
