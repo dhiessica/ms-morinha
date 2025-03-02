29d35cfa28e7c6f4206bdea7e5e0a57bc4c7ecab
dhiessicamoreira <dhiessicamoreira@hotmail.com>
2025-03-02 12:38:36 -0300

    Autorização

--- src/main/kotlin/br/pucpr/authserver/security/JWT.kt
+++ src/main/kotlin/br/pucpr/authserver/security/JWT.kt
@@ -2,9 +2,15 @@ package br.pucpr.authserver.security
 
 import br.pucpr.authserver.users.User
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.jackson.io.JacksonDeserializer
 import io.jsonwebtoken.jackson.io.JacksonSerializer
 import io.jsonwebtoken.security.Keys
+import jakarta.servlet.http.HttpServletRequest
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.stereotype.Component
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
@@ -28,6 +34,28 @@ class JWT {
                 .compact()
         }
 
+    fun extract(req: HttpServletRequest): Authentication? {
+        try {
+            val header = req.getHeader(AUTHORIZATION)
+            if (header == null || !header.startsWith("Bearer ")) return null
+            val token = header.removePrefix("Bearer ")
+            if (token.isEmpty()) return null
+
+            val claims = Jwts.parser().json(JacksonDeserializer(mapOf(USER_FIELD to UserToken::class.java)))
+                .verifyWith(Keys.hmacShaKeyFor(SECRET.toByteArray()))
+                .build()
+                .parseSignedClaims(token).payload
+
+            if (claims.issuer != ISSUER) return null
+            return claims.get("user", UserToken::class.java).toAuthentication()
+
+
+        } catch (e: Throwable) {
+            log.warn("Token rejected", e)
+            return null
+        }
+    }
+
     companion object {
         val log = LoggerFactory.getLogger(JWT::class.java)
 
@@ -40,5 +68,9 @@ class JWT {
 
         private fun utcNow() = ZonedDateTime.now(ZoneOffset.UTC)
         private fun ZonedDateTime.toDate():Date = Date.from(this.toInstant())
+        private fun UserToken.toAuthentication(): Authentication {
+            val authorities = this.roles.map { SimpleGrantedAuthority("ROLE_$it") }
+            return UsernamePasswordAuthenticationToken(this, id, authorities)
+        }
     }
 }
